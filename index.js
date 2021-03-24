c9b0292975caa1041c62a53e63d0a1c3c1fef294
muratcanoncu <murat.oncu1992@hotmail.com>
2021-03-24 13:56:37 +0100

    styling update

--- index.js
+++ index.js
@@ -19,31 +19,42 @@ let add = document.querySelector("#add");
 let dot = document.querySelector("#dot");
 let equal = document.querySelector(".equal");
 let erase = document.querySelector(".delete");
+let deleteAll = document.querySelector(".deleteAll");
 
 let screen = document.querySelector(".screen");
+let display = document.querySelector(".display");
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
+function actionListener() {
+  action.push(Number(screen.textContent));
+  action.push("*");
+  screen.textContent = "";
+  display.append("*");
+}
 let action = [];
 // actions EventListener
 multiply.addEventListener("click", function () {
   action.push(Number(screen.textContent));
   action.push("*");
   screen.textContent = "";
+  display.append("*");
 });
 divide.addEventListener("click", function () {
   action.push(Number(screen.textContent));
   action.push("/");
   screen.textContent = "";
+  display.append("/");
 });
 minus.addEventListener("click", function () {
   action.push(Number(screen.textContent));
   action.push("-");
   screen.textContent = "";
+  display.append("-");
 });
 add.addEventListener("click", function () {
   action.push(Number(screen.textContent));
   action.push("+");
   screen.textContent = "";
+  display.append("+");
 });
 // Equal Button
 equal.addEventListener("click", function () {
@@ -57,48 +68,71 @@ equal.addEventListener("click", function () {
   } else if (action[1] == "+") {
     result = action[0] + Number(screen.textContent);
   }
-  screen.textContent = "";
-  screen.append(result);
+  screen.textContent = " ";
+  if (!result) {
+    screen.append(" ");
+  } else {
+    display.textContent = "";
+    display.append(`${result},`);
+  }
+  action = [];
 });
 
 // numbers EventListener
 nine.addEventListener("click", function () {
   screen.append(numbers[9]);
+  display.append(numbers[9]);
 });
 eight.addEventListener("click", function () {
   screen.append(numbers[8]);
+  display.append(numbers[8]);
 });
 seven.addEventListener("click", function () {
   screen.append(numbers[7]);
+  display.append(numbers[7]);
 });
 six.addEventListener("click", function () {
   screen.append(numbers[6]);
+  display.append(numbers[6]);
 });
 five.addEventListener("click", function () {
   screen.append(numbers[5]);
+  display.append(numbers[5]);
 });
 four.addEventListener("click", function () {
   screen.append(numbers[4]);
+  display.append(numbers[4]);
 });
 three.addEventListener("click", function () {
   screen.append(numbers[3]);
+  display.append(numbers[3]);
 });
 two.addEventListener("click", function () {
   screen.append(numbers[2]);
+  display.append(numbers[2]);
 });
 one.addEventListener("click", function () {
   screen.append(numbers[1]);
+  display.append(numbers[1]);
 });
 zero.addEventListener("click", function () {
   screen.append(numbers[0]);
+  display.append(numbers[0]);
 });
 dot.addEventListener("click", function () {
   screen.append(".");
+  display.append(".");
 });
 // Delete Button
 erase.addEventListener("click", function () {
   screen.innerHTML = " ";
+
   for (let i = 0; i < 3; i++) {
     action.pop();
   }
+});
+deleteAll.addEventListener("click", function () {
+  screen.innerHTML = " ";
+  display.innerHTML = " ";
+  action = [];
 });
